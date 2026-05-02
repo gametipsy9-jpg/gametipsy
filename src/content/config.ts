8a81f5b0b612b5c9a1bfa94dba74cc20c8369e41
gametipsy9-jpg <gametipsy9@gmail.com>
2026-05-02 09:56:08 -0300

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,17 @@
 import { defineCollection, z } from "astro:content";
 
-const showcase = defineCollection({
-  type: "data",
-  schema: ({ image }) =>
-    z.object({
-      title: z.string().min(1),
-      image: image(),
-      url: z.string().url(),
-      featured: z.number().min(1).optional(),
-    }),
+const blog = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    category: z.enum(["Random Finds", "Hidden Gems"]),
+    label: z.enum(["Worth Trying", "Recommended"]).optional(),
+    thumbnail: z.string().optional(),
+    excerpt: z.string().optional(),
+  }),
 });
 
 export const collections = {
-  showcase,
+  blog,
 };
